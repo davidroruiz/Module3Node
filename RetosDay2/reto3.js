c1f3712fe63f24b564a5becef5a94d31a06983fb
David Rodriguez Ruiz <davidroruiz@gmail.com>
2021-08-04 00:48:36 +0200

    feat(cambios): cambios de retos del Day2

--- RetosDay2/reto3.js
+++ RetosDay2/reto3.js
@@ -1,32 +1,45 @@
 const readline = require('readline');
 const fs = require('fs/promises');
+const { resolve } = require('path');
+
 
 let objectJSON = {};
 
+let nameMi = "";
+let surname = "";
+let age = "";
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-function question() {
-    const question = new Promise((resolve) => {
-        rl.question('Cual es tu nombre? ', (nameMi) => {
-            resolve(nameMi)
-            rl.question('Cual es tu apellido? ', (apellidoMi) => {
-                resolve(apellidoMi)
-                rl.question('Cual es tu edad? ', (edadMi) => {
-                    resolve(edadMi)
-                    objectJSON = { "name": nameMi, "surname": apellidoMi, "age": edadMi };
-                    ReadWriterDeleteEnFicheroJSON("miJSON");
-
-                    rl.close();
-                });
-            });
-
-        });
+async function pregunta(pregunta){
+    const question = new Promise((resolve,rejects)=>{
+        rl.question(pregunta,(respuesta)=>{resolve(respuesta)})
     })
-
     return question;
+}
+async function questionDatos(){
+    try{
+
+        let namePregunta =await pregunta("Cual es el nombre? ")
+        nameMi = namePregunta;
+        let apellidoPregunta =await pregunta("Cual es el apellido? ")
+        surname = apellidoPregunta;
+        let edadPregunta = await pregunta("Cual es la edad? ")
+        age = edadPregunta;
+        objectJSON = {
+
+            "nameMi": nameMi,
+            "surname": surname,
+            "age": age
+        };
+        rl.close();
+        ReadWriterDeleteEnFicheroJSON("miJSON");
+    }catch (err) {
+        console.log(err);
+
+    }
 }
 
 async function ReadWriterDeleteEnFicheroJSON(nombreFichero) {
@@ -38,7 +51,8 @@ async function ReadWriterDeleteEnFicheroJSON(nombreFichero) {
         console.log(JSON.parse(nuevaEscritura));
     } catch (err) {
         console.log(err);
+
     }
 }
 
-question();
+questionDatos();
